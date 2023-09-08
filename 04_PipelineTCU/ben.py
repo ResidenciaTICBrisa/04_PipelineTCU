5e94789bcff97a26c6a8f4b7f3d7aa0159d15f6a
Victor-oss <victorio.lazaro15@gmail.com>
2023-09-08 11:59:13 -0300

    (#129) Adição script de web-scrapping para os dados do BEN de 1970 até 2022 e  script que reúne os dados de todos os anos em uma única tabela. A tabela resultante que será passada para o banco se localiza na pasta constants com onome BEN_total.csv e o arquivo dos dados antes do tratamento é o BEN.xlsx

--- 04_PipelineTCU/ben.py
+++ 04_PipelineTCU/ben.py
@@ -1,10 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 import pathlib
 
 if __name__ == '__main__':
-    url = "https://www.epe.gov.br/sites-pt/publicacoes-dados-abertos/publicacoes/PublicacoesArquivos/publicacao-748/topico-678/Anexo IX - Balanços Energéticos Consolidados (em tep) 1970 - 2022.xlsx"
+    url = "https://www.epe.gov.br/pt/publicacoes-dados-abertos/publicacoes/BEN-Series-Historicas-Completas"
+    url_epe = "https://www.epe.gov.br"
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
+    soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
     arquivo = "BEN.xlsx" 
-    r = requests.get(url, allow_redirects=True)
+    a = soup.select_one('[href*="Matrizes Consolidadas"]')
+    link = url_epe + a['href']
+    r = requests.get(link, allow_redirects=True)
     output = open(arquivo, 'wb')
     output.write(r.content)
     output.close()
